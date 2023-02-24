1c4dc0d16feecc2df7794ecd60260f4e98e92ef9
심지훈 <simjihun@simjihuns-MacBook-Air.local>
2023-02-24 13:16:05 +0900

    0. Project Setting

---
+++ src/test/java/hello/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,14 @@
+package hello.springtx;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@Slf4j
+@SpringBootTest
+class SpringtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
